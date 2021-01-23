9cadd3c1155f9e678b007193ea77632e800a3e7b
Your Name <mashour365@gmail.com>
2021-01-23 21:20:11 +0200

    Added JSON support

--- lib/places_recommendation.dart
+++ lib/places_recommendation.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:path/path.dart';
 import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
@@ -13,30 +15,61 @@ class PlacesRecommendation extends StatefulWidget {
 
 class _PlacesRecommendationState extends State<PlacesRecommendation> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    pickFile(context);
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: FutureBuilder(
+          future: pickFile(context),
+          builder: (d, sna) => Container(
+            child: Center(
+              child: Swiper(
+                itemBuilder: (d, s) => Container(
+                  child: Text('${sna.data[s]['Name']}'),
+                ),
+                itemCount: sna.data.length,
+                autoplay: true,
+                fade: 0.9,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 
-  pickFile() async {
-    final input = new File().openRead();
-    final fields = await input
-        .transform(utf8.decoder)
-        .transform(new CsvToListConverter())
-        .toList();
-
-    print(fields);
+  pickFile(context) async {
+    String data =
+        await DefaultAssetBundle.of(context).loadString("assets/places.json");
+    final jsonResult = json.decode(data);
+    return jsonResult;
   }
-}
 
-assetToFile({String assetPath}) async {
-  Directory directory = await getApplicationDocumentsDirectory();
+  Future<Map<String, dynamic>> parseJsonFromAssets(String assetsPath) async {
+    print('--- Parse json from: $assetsPath');
+    return rootBundle
+        .loadString(assetsPath)
+        .then((jsonStr) => jsonDecode(jsonStr));
+  }
 
-  var path = join(directory.path, assetPath.split('/')[2]);
-  ByteData data = await rootBundle.load(assetPath);
-  List<int> bytes =
-      data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-  var file = await File(path).writeAsBytes(bytes);
-  return file.path;
+  assetToFile({String assetPath}) async {
+    Directory directory = await getApplicationDocumentsDirectory();
+
+    var path = join(directory.path, assetPath.split('/')[2]);
+    ByteData data = await rootBundle.load(assetPath);
+    List<int> bytes =
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    var file = await File(path).writeAsBytes(bytes);
+    return file.path;
+  }
 }

1d7bc809b05c9ae7559e99e565c3f3ca7389a977
Your Name <mashour365@gmail.com>
2021-01-24 03:28:04 +0200

    Refactored

---
+++ lib/Screens/places_list.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:animate_do/animate_do.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PlacesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FutureBuilder(
+        future: pickFile(context),
+        builder: (_, AsyncSnapshot snapshot) => Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Swiper(
+              curve: Curves.decelerate,
+              itemBuilder: (BuildContext context, int index) => Container(
+                child: Center(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.network(
+                          '${snapshot.data[index]['Image']}',
+                          height: 300,
+                        ),
+                      ),
+                      FadeInUp(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '${snapshot.data[index]['Name']}',
+                            style: TextStyle(color: Colors.white, fontSize: 21),
+                          ),
+                        ),
+                      ),
+                      FlatButton(
+                        child: Container(
+                          width: 300,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Check on Google Maps',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 19),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Icon(Icons.map_rounded),
+                              )
+                            ],
+                          ),
+                        ),
+                        onPressed: () => _launchURL(
+                            '${snapshot.data[index]['Longitude']}',
+                            '${snapshot.data[index]['Latitude']}'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              itemCount: snapshot.data.length,
+              autoplay: true,
+              duration: 6,
+              itemWidth: 400,
+              fade: 0.3,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  getData(index) async {
+    Response response;
+    Dio dio = Dio();
+    response = await dio.get("http://10.0.2.2:5000/api/$index");
+    print(response);
+  }
+
+  pickFile(context) async {
+    String data =
+        await DefaultAssetBundle.of(context).loadString("assets/places.json");
+    final jsonResult = json.decode(data);
+    return jsonResult;
+  }
+
+  _launchURL(long, lat) async {
+    var uri = Uri.parse("google.navigation:q=$lat,$long&mode=d");
+    if (await canLaunch(uri.toString())) {
+      await launch(uri.toString());
+    } else {
+      throw 'Could not launch ${uri.toString()}';
+    }
+  }
+}
--- lib/Screens/places_recommendation.dart
+++ lib/Screens/places_recommendation.dart
@@ -1,11 +1,4 @@
-import 'dart:convert';
-
-import 'package:animate_do/animate_do.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class PlacesRecommendation extends StatefulWidget {
   @override
@@ -15,7 +8,6 @@ class PlacesRecommendation extends StatefulWidget {
 class _PlacesRecommendationState extends State<PlacesRecommendation> {
   @override
   Widget build(BuildContext context) {
-    pickFile(context);
     return Scaffold(
       backgroundColor: Color(0xff47516C),
       body: SafeArea(
@@ -30,117 +22,75 @@ class _PlacesRecommendationState extends State<PlacesRecommendation> {
                     width: 150,
                     height: 50,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.red),
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          colors: [
+                            Color(0xff7558FF),
+                            Color(0xff3396FF),
+                            Color(0xff1AB0FF)
+                          ]),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.place,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'Ratings',
+                          style: TextStyle(fontSize: 21, color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
                   Container(
                     width: 150,
                     height: 50,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.red),
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          colors: [
+                            Color(0xffFFC300),
+                            Color(0xffFFB400),
+                            Color(0xffFF9E00)
+                          ]),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.trip_origin_rounded,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Trip',
+                          style: TextStyle(
+                              fontSize: 21,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FutureBuilder(
-                  future: pickFile(context),
-                  builder: (_, AsyncSnapshot snapshot) => Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.width,
-                    child: Center(
-                      child: Swiper(
-                        curve: Curves.decelerate,
-                        itemBuilder: (BuildContext context, int index) =>
-                            Container(
-                          child: Center(
-                            child: Column(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(30),
-                                  child: Image.network(
-                                    '${snapshot.data[index]['Image']}',
-                                    height: 300,
-                                  ),
-                                ),
-                                FadeInUp(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      '${snapshot.data[index]['Name']}',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 21),
-                                    ),
-                                  ),
-                                ),
-                                FlatButton(
-                                  child: Container(
-                                    width: 300,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'Check on Google Maps',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 19),
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.all(3.0),
-                                          child: Icon(Icons.map_rounded),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  onPressed: () => _launchURL(
-                                      '${snapshot.data[index]['Longitude']}',
-                                      '${snapshot.data[index]['Latitude']}'),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        itemCount: snapshot.data.length,
-                        autoplay: true,
-                        duration: 6,
-                        itemWidth: 400,
-                        fade: 0.3,
-                      ),
-                    ),
-                  ),
-                ),
+              Text(
+                'Places in Ismailia to rate!',
+                style: TextStyle(fontSize: 21, color: Colors.white),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  getData(index) async {
-    Response response;
-    Dio dio = Dio();
-    response = await dio.get("http://10.0.2.2:5000/api/$index");
-    print(response);
-  }
-
-  pickFile(context) async {
-    String data =
-        await DefaultAssetBundle.of(context).loadString("assets/places.json");
-    final jsonResult = json.decode(data);
-    return jsonResult;
-  }
-
-  _launchURL(long, lat) async {
-    var uri = Uri.parse("google.navigation:q=$lat,$long&mode=d");
-    if (await canLaunch(uri.toString())) {
-      await launch(uri.toString());
-    } else {
-      throw 'Could not launch ${uri.toString()}';
-    }
   }
 }

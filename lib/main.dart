716f04356051d280e300c656e3910a19a4848999
Your Name <mashour365@gmail.com>
2021-01-24 03:15:07 +0200

    Added Noyh font

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:places_recommendation/places_recommendation.dart';
+import 'package:places_recommendation/Screens/places_recommendation.dart';
 
-main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: PlacesRecommendation(),
+      theme: ThemeData(fontFamily: 'Noyh'),
     );
   }
 }
